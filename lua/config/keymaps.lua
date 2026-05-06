5d7df212938519983ebf4afc42d24dd59d3d0d6c
yy <yy1875737985@outlook.com>
2026-05-06 19:13:18 +0800

    feat(keymap): 添加 jj 推出插入模式

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- 使用 jj 退出插入模式
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
