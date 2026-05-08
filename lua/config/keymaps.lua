77646f72abd2e3e9ede4ca930cc6fc5fcc5f50b8
yy <yy1875737985@outlook.com>
2026-05-08 10:31:42 +0800

    fix(keymap):  jj 映射仅在非 lazygit 终端中生效
    避免在 lazygit 中因 jj 映射导致输入延迟

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,14 @@
 -- 使用 jj 退出插入模式
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
 
--- 退出终端模式
-vim.keymap.set("t", "jj", "<C-\\><C-n>", { desc = "Exit terminal insert mode" })
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal insert mode" })
+-- 退出终端插入模式
+-- 只在非 lazygit 终端中创建 jj 映射（终端打开时动态创建）
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function(args)
+    local bufname = vim.api.nvim_buf_get_name(args.buf)
+    if not bufname:match("lazygit") then
+      vim.keymap.set("t", "jj", "<C-\\><C-n>", { buffer = args.buf, desc = "Exit terminal mode" })
+    end
+  end,
+})
