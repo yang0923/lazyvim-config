5fc6d64f126ec08a7bf28497f81920b624c4ffd5
yy <yy1875737985@outlook.com>
2026-05-20 11:27:49 +0800

    feat: xxxxx

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        documentation = {
+          treesitter_highlighting = false, -- 关闭文档高亮
+        },
+      },
+      signature = {
+        window = {
+          treesitter_highlighting = false, -- 关闭签名帮助高亮
+        },
+      },
+    },
+  },
+}
